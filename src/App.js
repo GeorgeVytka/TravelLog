1e7274b6a99dc9e92c15094eb4ac8a500df7cc26
LYSANDER-PC\Lysander <finblack20@gmai.com>
2020-07-08 10:12:08 -0400

    added a pin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import {ListLogEntries} from './API';
 const App = () =>  {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -16,7 +18,7 @@ const App = () =>  {
   useEffect( () => {
     ( async () => {
       const logEntries = await ListLogEntries();
-      console.log(logEntries);
+      setLogEntries(logEntries);
     }) ();
   },[] );
 
@@ -26,7 +28,61 @@ const App = () =>  {
       mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+      >
+        {
+        logEntries.map(entry => (  
+          <>
+        <Marker 
+          key={entry._id}
+          latitude={entry.latitude} 
+          longitude={entry.longitude} 
+         // offsetLeft={-20} 
+         // offsetTop={-10} 
+         style={{
+          height: `${6 * viewport.zoom}px`,
+          width: `${6 * viewport.zoom}px`,
+        }}
+         >
+           <div
+            onClick={() => setShowPopup({
+              // ...showPopup,
+              [entry._id]: true,
+            })}
+           >
+
+            <img
+             className="marker" 
+             src="https://i.imgur.com/y0G5YTX.png" 
+             alt="marker"/>
+           </div>
+           
+        </Marker>
+        
+         {
+          showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setShowPopup({})}
+              anchor="top" >
+              <div className="popup">
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+                <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                {entry.image && <img src={entry.image} alt={entry.title} />}
+              </div>
+            </Popup>
+          ) : null
+        }
+        </>
+        )
+        )
+        
+        }
+      </ReactMapGL>
   );
 }
 
