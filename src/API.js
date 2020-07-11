1cff74e8e192a2deaac231405c0889bf0459376a
LYSANDER-PC\Lysander <finblack20@gmai.com>
2020-07-11 08:20:30 -0400

    user now can make a entry with a api key

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 const API_URL = 'http://localhost:1337';
 
 
-export async function ListLogEntries(){
+export async function listLogEntries(){
     const response = await fetch(`${API_URL}/api/logs`);
     return  response.json();
 }
